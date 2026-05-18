747b46b11f8503cb9a796e89cda2f8992936c84a
Giannin <contact@giannin.ch>
2026-05-18 22:18:49 +0200

    feat: init flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake to show how nix and flakes can be used to package and distribute scripts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python3
+          ];
+        };
+      }
+    );
+}
